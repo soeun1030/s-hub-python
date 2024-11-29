0ca4c1cf82b39dddd2780e0f055a4236a6d55e90
soeun1030 <ssoo1030@tukorea.ac.kr>
2024-11-29 13:51:51 +0900

    11/29 practice

---
+++ 1.py
@@ -0,0 +1,16 @@
+print("hello world")
+print("hello", "Python", 1, 2, "asdfe", sep="\n")
+print()
+print("1")
+print("안녕하세요", end="")
+print("코딩온입니다.")
+
+ive = "I AM"
+print(ive)
+ive = "장원영"
+print(ive)
+Ive = "카리나"
+print(ive, Ive)
+my_home : snake_case
+MyHome : CamelCase
+myHome : PascalCase
