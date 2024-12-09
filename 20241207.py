8b9ad482c2c5c194de51dc066d3031d304cd6fce
soeun1030 <ssoo1030@tukorea.ac.kr>
2024-12-09 09:45:19 +0900

    class

--- 20241207.py
+++ 20241207.py
@@ -138,3 +138,64 @@ def solution(t, p):
 
 # 연습7. 콜라츠 수열 만들기
 # https://school.programmers.co.kr/learn/courses/30/lessons/181919
+def solution(n):
+    answer = []
+
+    def collat(x):
+        answer.append(x)
+        if x == 1:
+            return
+        elif x % 2 == 0:
+            collat(x/2)
+        else:
+            collat(x*3+1)
+    collat(n)
+    return answer
+
+
+# 숙련자용1. 특이한 정렬
+# https://school.programmers.co.kr/learn/courses/30/lessons/120880
+def solution(numlist, n):
+    answer = []
+    d = {}
+    for i in numlist:
+        d[i] = abs(i-n)
+    d1 = sorted(d.items(), key = lambda item:(item[1], -item[0]))
+    print(d.items())
+    print(d1)
+    for i in d1:
+        answer.append(i[0])
+    return answer
+
+
+# 숙련자용2. 옹알이(1)
+# https://school.programmers.co.kr/learn/courses/30/lessons/120956
+def solution(babbling):
+    answer = 0
+    can = ["aya", "ye", "woo", "ma"]
+    for i in babbling:
+        for j in can:
+            idx = i.find(j)
+            if idx > -1:
+                print(idx, i, j)
+                i = i.replace(j, '_')
+                print(idx, i, j)
+        i = i.replace('_', '')
+        if len(i) == 0:
+            answer += 1
+    return answer
+
+
+# 숙련자용3. 하노이의 탑
+# https://school.programmers.co.kr/learn/courses/30/lessons/12946
+def solution(n):
+    answer = []
+    def hanoi(n, f, t, v):
+        if n == 1:
+            answer.append([f,t])
+        else:
+            hanoi(n-1, f , v, t)
+            answer.append([f,t])
+            hanoi(n-1, v, t, f)
+    hanoi(n, 1 , 3, 2)
+    return answer
